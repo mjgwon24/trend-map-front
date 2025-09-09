c4190b35eeded0d51d918a8b9248c96d17bfc75c
gwon minji <alswlchlrh8@naver.com>
2025-09-10 00:05:12 +0900

    feat: Add custom font and theme configuration for the application

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,20 @@
 import type { Config } from "tailwindcss";
+import colors, { generateTailwindColors } from './src/theme/colors';
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      colors: generateTailwindColors(colors),
     },
   },
   plugins: [],
 };
+
 export default config;
